881e1771a5fb5b6aa08219d73553f09561187da6
Lucas S <lucas.sorribes@gmail.com>
2023-01-09 13:42:24 +0100

    Refactor CastlingBoardControl#castle! and uncastle! to remove nested ifs

--- lib/movement_rules/castling_board_control.rb
+++ lib/movement_rules/castling_board_control.rb
@@ -7,44 +7,34 @@ module CastlingBoardControl
   end
 
   def castle!(side, color, permanent=false)
-    case color
-    when :white
-      if side == :king
-        move_piece!([7, 4], [7, 6], permanent)
-        move_piece!([7, 7], [7, 5], permanent)
-      else
-        move_piece!([7, 4], [7, 2], permanent)
-        move_piece!([7, 0], [7, 3], permanent)
-      end
+    if color == :white && side == :king
+      move_piece!([7, 4], [7, 6], permanent)
+      move_piece!([7, 7], [7, 5], permanent)
+    elsif color == :white && side == :queen
+      move_piece!([7, 4], [7, 2], permanent)
+      move_piece!([7, 0], [7, 3], permanent)
+    elsif color == :black && side == :king
+      move_piece!([0, 4], [0, 6], permanent)
+      move_piece!([0, 7], [0, 5], permanent)
     else
-      if side == :king
-        move_piece!([0, 4], [0, 6], permanent)
-        move_piece!([0, 7], [0, 5], permanent)
-      else
-        move_piece!([0, 4], [0, 2], permanent)
-        move_piece!([0, 0], [0, 3], permanent)
-      end
+      move_piece!([0, 4], [0, 2], permanent)
+      move_piece!([0, 0], [0, 3], permanent)
     end
   end
 
   def uncastle!(side, color)
-    case color
-    when :white
-      if side == :king
-        move_piece!([7, 6], [7, 4])
-        move_piece!([7, 5], [7, 7])
-      else
-        move_piece!([7, 2], [7, 4])
-        move_piece!([7, 3], [7, 0])
-      end
+    if color == :white && side == :king
+      move_piece!([7, 6], [7, 4])
+      move_piece!([7, 5], [7, 7])
+    elsif color == :white && side == :queen
+      move_piece!([7, 2], [7, 4])
+      move_piece!([7, 3], [7, 0])
+    elsif color == :black && side == :king
+      move_piece!([0, 6], [0, 4])
+      move_piece!([0, 5], [0, 7])
     else
-      if side == :king
-        move_piece!([0, 6], [0, 4])
-        move_piece!([0, 5], [0, 7])
-      else
-        move_piece!([0, 2], [0, 4])
-        move_piece!([0, 3], [0, 0])
-      end
+      move_piece!([0, 2], [0, 4])
+      move_piece!([0, 3], [0, 0])
     end
   end
 end
