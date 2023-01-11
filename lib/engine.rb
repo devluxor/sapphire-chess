fd3020973d9bc3d136b7deeff4a3102447819100
Lucas S <lucas.sorribes@gmail.com>
2023-01-11 13:19:25 +0100

    Add movement_rules.rb

--- lib/engine.rb
+++ lib/engine.rb
@@ -6,6 +6,8 @@ require_relative 'display.rb'
 require_relative 'human_input_validation.rb'
 require_relative 'algebraic_conversion.rb'
 
+require 'paint'
+
 class Engine
   include Display
   include HumanInputValidation
---
+++ lib/movement_rules.rb
@@ -0,0 +1,7 @@
+require_relative 'movement_rules/castling_board_control.rb'
+require_relative 'movement_rules/castling_piece_control.rb'
+require_relative 'movement_rules/castling_rights.rb'
+require_relative 'movement_rules/en_passant_board_control.rb'
+require_relative 'movement_rules/en_passant_piece_control.rb'
+require_relative 'movement_rules/move_slide_pattern.rb'
+require_relative 'movement_rules/move_step_pattern.rb'
