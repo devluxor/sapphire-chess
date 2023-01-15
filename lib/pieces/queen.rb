5a455dec4d6a1d53c150d62bec4505bddcfc47c5
Lucas S <lucas.sorribes@gmail.com>
2023-01-15 13:58:59 +0100

    Rubocop queen.rb

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,11 +2,11 @@ class Queen < Piece
   MOVE_DIRECTIONS = [
     [0, 1], [0, -1], [1, 0], [-1, 0],
     [1, 1], [1, -1], [-1, 1], [-1, -1]
-  ]
-  
+  ].freeze
+
   BLACK = '♛'
   WHITE = '♕'
-  
+
   VALUE = 900
 
   WHITE_LOCATION_VALUE = [
@@ -18,7 +18,7 @@ class Queen < Piece
     [-10, 5, 5, 5, 5, 5, 0, -10],
     [-10, 0, 5, 0, 0, 0, 0, -10],
     [-20, -10, -10, -5, -5, -10, -10, -20]
-  ]
+  ].freeze
 
   BLACK_LOCATION_VALUE = [
     [-20, -10, -10, -5, -5, -10, -10, -20],
@@ -29,13 +29,13 @@ class Queen < Piece
     [-10, 0, 5, 5, 5, 5, 0, -10],
     [-10, 0, 0, 0, 0, 0, 0, -10],
     [-20, -10, -10, -5, -5, -10, -10, -20]
-  ]
-    
+  ].freeze
+
   include SlidePattern
 
   def location_value
     row, column = location
-    
+
     case color
     when :white then self.class::WHITE_LOCATION_VALUE[row][column]
     else self.class::BLACK_LOCATION_VALUE[row][column]
