f8e79651e75b534ad304e229a7b74ca7e56f800f
Lucas S <lucas.sorribes@gmail.com>
2023-01-15 14:35:08 +0100

    Rubocop ai.rb

--- lib/ai.rb
+++ lib/ai.rb
@@ -4,22 +4,22 @@ module AI
   # Chooses move by best possible outcome:
   def computer_chooses_move
     possible_moves = board.generate_moves(color)
-    possible_moves << [:castle, :king] if castle_rights?(:king)
-    possible_moves << [:castle, :queen] if castle_rights?(:queen)
+    possible_moves << %i[castle king] if castle_rights?(:king)
+    possible_moves << %i[castle queen] if castle_rights?(:queen)
 
-    best_move(possible_moves, color)
+    best_move(possible_moves)
   end
 
-  def best_move(possible_moves, color)
+  def best_move(possible_moves)
     evaluations = {}
 
     anti_loop_filter(possible_moves)
 
     possible_moves.each do |move|
-      evaluations[move] = 
-      minimax(move, depth, -Float::INFINITY, Float::INFINITY, maximizing_player?)
+      evaluations[move] =
+        minimax(move, depth, -Float::INFINITY, Float::INFINITY, maximizing_player?)
     end
-    
+
     move_randomizer(evaluations)
   end
 
@@ -27,57 +27,59 @@ module AI
     return board.evaluate if depth.zero?
 
     piece_buffer = board[move.last] unless move.first == :castle
-    
+
     board.make_provisional!(move, color)
 
     # This generates possible outcomes (children) for the provisional move:
-    # Each branch represents the next turn (i.e.: if current player is white 
+    # Each branch represents the next turn (i.e.: if current player is white
     # [the maximizing player], it generates every possible movement for the
-    # next player, black [the minimizing player], who will choose the best 
-    # possible move, and so on. The best (relative to each player) possible 
+    # next player, black [the minimizing player], who will choose the best
+    # possible move, and so on. The best (relative to each player) possible
     # outcome for each move will determine what move is chosen, `best_evaluation`)
     # See AI#computer_chooses_move
 
     # The alpha-beta `prunes` the tree: it makes the search more efficient
     # removing unnecessary branches, resulting in a faster process.
-    best_evaluation = if maximizing_player
-                        best_evaluation = Float::INFINITY
+    move_final_evaluation =
+      if maximizing_player
+        best_minimizing_evaluation = Float::INFINITY
 
-                        board.generate_moves(:black).each do |move| 
-                          evaluation = minimax(move, depth - 1, alpha, beta, false)
-                          best_evaluation = [best_evaluation, evaluation].min
-                          beta = [beta, evaluation].min
-                          break if beta <= alpha
-                        end
+        board.generate_moves(:black).each do |possible_move|
+          evaluation = minimax(possible_move, depth - 1, alpha, beta, false)
+          best_minimizing_evaluation = [best_minimizing_evaluation, evaluation].min
+          beta = [beta, evaluation].min
+          break if beta <= alpha
+        end
 
-                        best_evaluation
-                      else
-                        best_evaluation = -Float::INFINITY
+        best_minimizing_evaluation
+      else
+        best_maximizing_evaluation = -Float::INFINITY
 
-                        board.generate_moves(:white).each do |move| 
-                          evaluation = minimax(move, depth - 1, alpha, beta, true)
-                          best_evaluation = [best_evaluation, evaluation].max
-                          alpha = [alpha, evaluation].max
-                          break if beta <= alpha
-                        end
+        board.generate_moves(:white).each do |possible_move|
+          evaluation = minimax(possible_move, depth - 1, alpha, beta, true)
+          best_maximizing_evaluation = [best_maximizing_evaluation, evaluation].max
+          alpha = [alpha, evaluation].max
+          break if beta <= alpha
+        end
 
-                        best_evaluation
-                      end
+        best_maximizing_evaluation
+      end
 
     board.unmake_provisional!(piece_buffer, move, color)
 
-    best_evaluation
+    move_final_evaluation
   end
 
   # This method randomizes the moves if two or more moves share the best evaluation.
   # This avoids the Computer to play the same moves every game.
   def move_randomizer(evaluations)
     best_evaluation =
-    if color == :white then evaluations.values.max
-    else evaluations.values.min
-    end
+      if color == :white then evaluations.values.max
+      else
+        evaluations.values.min
+      end
 
-    evaluations.select do |_, evaluation| 
+    evaluations.select do |_, evaluation|
       evaluation == best_evaluation
     end.keys.sample
   end
@@ -89,12 +91,13 @@ module AI
   # For evaluation analysis only:
   def store_evaluation(move, evaluation)
     return [move, evaluation] if move.first == :castle
+
     description = format(
-      "%s %s to %s %s",
-      board[move.first].class,
-      move.first,
-      board[move.last].class,
-      move.last
+      '%<piece_class>s %<piece_position>s to %<target_class>s %<target_position>s',
+      piece_class: board[move.first].class,
+      piece_position: move.first,
+      target_class: board[move.last].class,
+      target_position: move.last
     )
 
     [description, evaluation]
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,11 +1,11 @@
 class Rook < Piece
   MOVE_DIRECTIONS = [
     [0, 1], [0, -1], [1, 0], [-1, 0]
-  ]
+  ].freeze
 
   BLACK = '♜'
   WHITE = '♖'
-  
+
   VALUE = 500
 
   WHITE_LOCATION_VALUE = [
@@ -17,7 +17,7 @@ class Rook < Piece
     [-5, 0, 0, 0, 0, 0, 0, -5],
     [-5, 0, 0, 0, 0, 0, 0, -5],
     [0, 0, 0, 5, 5, 0, 0, 0]
-  ]
+  ].freeze
 
   BLACK_LOCATION_VALUE = [
     [0, 0, 0, 5, 5, 0, 0, 0],
@@ -28,7 +28,7 @@ class Rook < Piece
     [-5, 0, 0, 0, 0, 0, 0, -5],
     [5, 10, 10, 10, 10, 10, 10, 5],
     [0, 0, 0, 0, 0, 0, 0, 0]
-  ]
+  ].freeze
 
   WHITE_LOCATION_VALUE_EASY = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -39,7 +39,7 @@ class Rook < Piece
     [-5, 0, 10, 10, 10, 10, 0, -5],
     [-5, 0, 0, 0, 0, 0, 0, -5],
     [0, 0, 0, 5, 5, 0, 0, 0]
-  ]
+  ].freeze
 
   BLACK_LOCATION_VALUE_EASY = [
     [0, 0, 0, 5, 5, 0, 0, 0],
@@ -50,7 +50,7 @@ class Rook < Piece
     [-5, 0, 10, 10, 10, 10, 0, -5],
     [5, 10, 10, 10, 10, 10, 10, 5],
     [0, 0, 0, 0, 0, 0, 0, 0]
-  ]
+  ].freeze
 
   include SlidePattern
   include CastlingPieceControl
