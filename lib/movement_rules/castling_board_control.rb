8643020aff118bff9c19e46246ce81bea07ca887
Lucas S <lucas.sorribes@gmail.com>
2023-01-15 17:40:18 +0100

    Refactor CastlingBoardControl#castle! and #uncastle!

--- lib/movement_rules/castling_board_control.rb
+++ lib/movement_rules/castling_board_control.rb
@@ -7,34 +7,31 @@ module CastlingBoardControl
   end
 
   def castle!(side, color, permanent: false)
-    if color == :white && side == :king
-      move_piece!([7, 4], [7, 6], permanent: permanent)
-      move_piece!([7, 7], [7, 5], permanent: permanent)
-    elsif color == :white && side == :queen
-      move_piece!([7, 4], [7, 2], permanent: permanent)
-      move_piece!([7, 0], [7, 3], permanent: permanent)
-    elsif color == :black && side == :king
-      move_piece!([0, 4], [0, 6], permanent: permanent)
-      move_piece!([0, 7], [0, 5], permanent: permanent)
-    else
-      move_piece!([0, 4], [0, 2], permanent: permanent)
-      move_piece!([0, 0], [0, 3], permanent: permanent)
-    end
+    castling_squares =
+      if color == :white && side == :king then [[7, 4], [7, 6], [7, 7], [7, 5]]
+      elsif color == :white && side == :queen then [[7, 4], [7, 2], [7, 0], [7, 3]]
+      elsif color == :black && side == :king then [[0, 4], [0, 6], [0, 7], [0, 5]]
+      else
+        [[0, 4], [0, 2], [0, 0], [0, 3]]
+      end
+
+    king_start, king_target, rook_start, rook_target = castling_squares
+
+    move_piece!(king_start, king_target, permanent: permanent)
+    move_piece!(rook_start, rook_target, permanent: permanent)
   end
 
   def uncastle!(side, color)
-    if color == :white && side == :king
-      move_piece!([7, 6], [7, 4], permanent: false)
-      move_piece!([7, 5], [7, 7], permanent: false)
-    elsif color == :white && side == :queen
-      move_piece!([7, 2], [7, 4], permanent: false)
-      move_piece!([7, 3], [7, 0], permanent: false)
-    elsif color == :black && side == :king
-      move_piece!([0, 6], [0, 4], permanent: false)
-      move_piece!([0, 5], [0, 7], permanent: false)
-    else
-      move_piece!([0, 2], [0, 4], permanent: false)
-      move_piece!([0, 3], [0, 0], permanent: false)
-    end
+    castling_squares =
+      if color == :white && side == :king then [[7, 6], [7, 4], [7, 5], [7, 7]]
+      elsif color == :white && side == :queen then [[7, 2], [7, 4], [7, 3], [7, 0]]
+      elsif color == :black && side == :king then [[0, 6], [0, 4], [0, 5], [0, 7]]
+      else
+        [[0, 2], [0, 4], [0, 3], [0, 0]]
+      end
+    king_start, king_target, rook_start, rook_target = castling_squares
+
+    move_piece!(king_start, king_target, permanent: false)
+    move_piece!(rook_start, rook_target, permanent: false)
   end
 end
